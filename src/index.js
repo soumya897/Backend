c7da8153a3afb2e38a04db5ff42b1b4798cf9e96
Soumya Ghosh <soumyaghosh825@gmail.com>
2025-07-20 11:49:28 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+});
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on('error', (error) => {
+            console.error("Error connecting to MongoDB:", error)
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+
+    }catch (error) {
+        console.error("Error connecting to MongoDB:", error)
+        throw error;
+    }   
+})()
+
+*/
